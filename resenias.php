3b7375175d347baafd1c99f12bf4e7e1094e3d1e
Ángel Aldaz <70903098+AngelAldaz@users.noreply.github.com>
2024-06-03 08:33:39 -0600

    Update resenias.php

--- resenias.php
+++ resenias.php
@@ -38,30 +38,40 @@ if (isset($_GET['movieID'])) {
       <h1 class="text-4xl font-bold">RESEÑAS</h1>
 
     </header>
-      <?php foreach ($registros as $registro) { ?>
-        <article class="w-2/4 mx-auto mt-5">
-          <div class="font-medium dark:text-white">
-            <p><?php echo $registro['nombre'] ?></p>
-          </div>
-          <div class="flex items-center mb-1 space-x-1 rtl:space-x-reverse">
-            <?php for ($i = 0; $i < $cantAM; $i++) { ?>
-              <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-            <?php } ?>
-            <?php for ($i = 0; $i < $cantNE; $i++) { ?>
-              <svg class="w-4 h-4 text-gray-300 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-            <?php } ?>
-          </div>
-          <footer class="mb-5 text-sm text-gray-500 dark:text-gray-400">
-            <p><?php echo $registro['correo'] ?></p>
-          </footer>
-          <p class="mb-2 text-gray-500 dark:text-gray-400"><?php echo $registro['comentario'] ?></p>
-        </article>
-      <?php } ?>
 
+    <?php
+    // Verificar si se encontraron comentarios
+    if (!$registro) {
+      // No se encontraron comentarios, mostrar un mensaje
+      echo "<h1 class='text-2xl font-bold mt-5 text-center'>No hay comentarios disponibles :(</h1>";
+    } else {
+      // Se encontraron comentarios, continuar con la visualización
+      $cantAM = ceil($registro['calificacion'] / 2);
+      $cantNE = 5 - $cantAM;
+    ?>
+      <?php foreach ($registros as $registro) { ?>
+      <article class="w-2/4 mx-auto mt-5">
+        <div class="font-medium dark:text-white">
+          <p><?php echo $registro['nombre'] ?></p>
+        </div>
+        <div class="flex items-center mb-1 space-x-1 rtl:space-x-reverse">
+          <?php for ($i = 0; $i < $cantAM; $i++) { ?>
+            <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+          <?php } ?>
+          <?php for ($i = 0; $i < $cantNE; $i++) { ?>
+            <svg class="w-4 h-4 text-gray-300 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+          <?php } ?>
+        </div>
+        <footer class="mb-5 text-sm text-gray-500 dark:text-gray-400">
+          <p><?php echo $registro['correo'] ?></p>
+        </footer>
+        <p class="mb-2 text-gray-500 dark:text-gray-400"><?php echo $registro['comentario'] ?></p>
+      </article>
+        <?php } ?>
 
   </body>
 
